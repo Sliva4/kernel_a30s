c41bda0131d3e1edb8c7c4c93f81dd8be12c5de2
Sliva4 <slivaktoto@gmail.com>
2025-10-30 16:21:34 +0700

    remove slivapatch 1

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -99,11 +99,6 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	struct path mnt_path = { .dentry = mnt->mnt_root, .mnt = mnt };
 	struct super_block *sb = mnt_path.dentry->d_sb;
 
-	if (is_suspicious_mount(mnt, &p->root)) {
-		err = SEQ_SKIP;
-		goto out;
-	}
-
 	if (sb->s_op->show_devname) {
 		err = sb->s_op->show_devname(m, mnt_path.dentry);
 		if (err)
@@ -139,11 +134,6 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	struct super_block *sb = mnt->mnt_sb;
 	struct path mnt_path = { .dentry = mnt->mnt_root, .mnt = mnt };
 	int err = 0;
-
-	if (is_suspicious_mount(mnt, &p->root)) {
-		err = SEQ_SKIP;
-		goto out;
-	}
 
 	seq_printf(m, "%i %i %u:%u ", r->mnt_id, r->mnt_parent->mnt_id,
 		   MAJOR(sb->s_dev), MINOR(sb->s_dev));
