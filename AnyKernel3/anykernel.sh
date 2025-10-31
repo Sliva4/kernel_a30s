38c0d565b10332a4f75373fdfb9a94f2278deaf4
Sliva4 <slivaktoto@gmail.com>
2025-10-31 15:17:06 +0700

    fix anykernel bugs

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -1,10 +1,10 @@
-### AnyKernel3 Ramdisk Mod Script
-## osm0sis @ xda-developers
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
 
-### AnyKernel setup
-# global properties
+## AnyKernel setup
+# begin properties
 properties() { '
-kernel.string=Sliva kernel installer.
+kernel.string=Sliva Kernel
 do.devicecheck=0
 do.modules=0
 do.systemless=1
@@ -17,93 +17,48 @@ device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
-supported.vendorpatchlevels=
 '; } # end properties
 
+# shell variables
+if [ -e /dev/block/platform/13500000.dwmmc0/by-name/BOOT ]; then
+	block=/dev/block/platform/13500000.dwmmc0/by-name/BOOT;
+elif [ -e /dev/block/platform/13500000.dwmmc0/by-name/boot ]; then
+	block=/dev/block/platform/13500000.dwmmc0/by-name/boot;
+fi
 
-### AnyKernel install
-## boot files attributes
-boot_attributes() {
-set_perm_recursive 0 0 755 644 $RAMDISK/*;
-set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-} # end attributes
+is_slot_device=0;
+ramdisk_compression=auto;
 
-# boot shell variables
-BLOCK=/dev/block/platform/13500000.dwmmc0/by-name/boot;
-IS_SLOT_DEVICE=0;
-RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
-
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-# boot install
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel install
+#Method 1:
+#dump_boot;
+#write_boot;
+
+#Method 2:
 split_boot;
-ui_print "+ Installing sliva kernel";
-ui_print " ";
+ui_print "- Installing Sliva kernel";
 flash_boot;
 
-ui_print "+ Installation finished successfully!";
 ui_print " ";
-## end boot install
+ui_print "- Installing/updating Sliva dtb";
+ui_print " ";
+flash_dtb;
 
+ui_print "- Installation finished successfully";
+ui_print " ";
 
-## init_boot files attributes
-#init_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $RAMDISK/*;
-#set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-#} # end attributes
-
-# init_boot shell variables
-#BLOCK=init_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
-
-# reset for init_boot patching
-#reset_ak;
-
-# init_boot install
-#dump_boot; # unpack ramdisk since it is the new first stage init ramdisk where overlay.d must go
-
-#write_boot;
-## end init_boot install
-
-
-## vendor_kernel_boot shell variables
-#BLOCK=vendor_kernel_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
-
-# reset for vendor_kernel_boot patching
-#reset_ak;
-
-# vendor_kernel_boot install
-#split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
-
-#flash_boot;
-## end vendor_kernel_boot install
-
-
-## vendor_boot files attributes
-#vendor_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $RAMDISK/*;
-#set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-#} # end attributes
-
-# vendor_boot shell variables
-#BLOCK=vendor_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-# vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-## end vendor_boot install
+ui_print "- Thank you for using Sliva Kernel :)";
+ui_print " ";
+## end install
 
