def676aa058b870c6808d922528c9b8ff7e79c8f
Sliva4 <slivaktoto@gmail.com>
2025-11-18 19:09:16 +0700

    Revert "fixes"
    This reverts commit cf512aa883201a66c18dffc209778dd047696ce0.

--- fs/namespace.c
+++ fs/namespace.c
@@ -32,6 +32,10 @@
 #include "pnode.h"
 #include "internal.h"
 
+#ifdef CONFIG_KSU
+#include <linux/ksu.h>
+#endif
+
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 extern bool susfs_is_current_ksu_domain(void);
 extern bool susfs_is_current_zygote_domain(void);
